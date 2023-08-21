038fa1ad0462ff35c4185a81d46fc9c3a1a3ae18
Zakttack <zak.merrigan@ndsu.edu>
2023-08-21 10:42:11 -0500

    Initialized Unit Test for Bowling Library

---
+++ BowlingLibraryTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace BowlingLibraryTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
